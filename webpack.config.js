d18b7b1645b67cb9c878e21a3aa117e137123528
mr-bat <abateni@hotmail.com>
2017-02-02 18:29:21 +0330

    initial step

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var config = {
+	entry: './main.js',
+	output: {
+		path: './',
+		filename: 'index.js'
+	},
+	devServer: {
+		inline : true,
+		port: 3434
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
+}
+
+module.exports = config;
